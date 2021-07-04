a1c09374be5b9242678b948b509e8e2ffa86e062
Junhua <wenjh1998@gmail.com>
2021-07-05 00:21:44 +0800

    Update settings.py

--- beyondhealthhack/beyondhealthhack/settings.py
+++ beyondhealthhack/beyondhealthhack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6w^bktj)@+gf*j(41oap0b3-1mee0#oz%ae=gv!37!t8h1ja8!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
