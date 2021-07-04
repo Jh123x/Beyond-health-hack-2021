f2066d7cf83589a425b6356643991f88378baf27
Junhua <wenjh1998@gmail.com>
2021-07-04 23:33:50 +0800

    Add basic pages for blog

--- beyondhealthhack/blog/views.py
+++ beyondhealthhack/blog/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse, HttpResponseNotFound
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from .models import Blog
 
 # Create your views here.
@@ -9,7 +9,7 @@ def index(request):
     if page <= 0:
         return HttpResponseNotFound("This page is not found")
     blog_posts = Blog.objects.all()[page-1:page+9]
-    return HttpResponse(f"Blog posts: {', '.join(map(lambda x: x.__str__(), blog_posts))}")
+    return render(request, 'blog_list.html', {'blogs': blog_posts})
 
 
 # Specific Blog page
@@ -18,6 +18,6 @@ def blog(request, id: int):
     blog = get_object_or_404(Blog, pk=id)
     blog.views += 1
     blog.save()
-    return HttpResponse(f"This is blog {blog.id}\nTitle: {blog.title}\nViews: {blog.views}\nLikes: {blog.likes}\nContents: \n{blog.content}\n", {"blog": blog})
+    return render(request, 'blog.html', {'blog': blog})
 
 
