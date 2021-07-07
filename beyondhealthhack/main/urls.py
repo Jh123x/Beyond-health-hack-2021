9bf2f21435389ea154727daf4867f6d02ea3ca07
Junhua <wenjh1998@gmail.com>
2021-07-07 23:48:00 +0800

    Add Sessions Joining + contact us

--- beyondhealthhack/main/urls.py
+++ beyondhealthhack/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register', views.register, name="register"),
     path('login', views.user_login, name="login"),
     path('logout', views.logout_view, name='logout'),
+    path('about_us', views.about_us, name='about_us')
 ]
