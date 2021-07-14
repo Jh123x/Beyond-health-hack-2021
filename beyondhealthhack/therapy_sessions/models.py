2ec14a5ea1f6c2248f0a12eaac96649c295f72e9
Junhua <wenjh1998@gmail.com>
2021-07-14 22:46:35 +0800

    Cleanup code

--- beyondhealthhack/therapy_sessions/models.py
+++ beyondhealthhack/therapy_sessions/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.constraints import CheckConstraint
 from django.db.models.deletion import CASCADE
 
 
